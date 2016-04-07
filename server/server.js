a57703330ea3e250e302de0578dba1fcf4ced29a
Neil Agarwal <nagar001@ucr.edu>
2016-04-07 14:19:27 -0700

    [Structure] set up Mongoose & Mongo, Inserted a User, Local Host will return that User

--- server/server.js
+++ server/server.js
@@ -3,19 +3,40 @@ var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-
+mongoose.connect('mongodb://localhost/rush');
+var Schema = mongoose.Schema;
 
 app.use(express.static(__dirname + '/../client'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {
+  // we're connected!
+  console.log('db connected');
+});
+
+var usersSchema = new Schema ({
+  email: String,
+  password: String,
+  isOwner: Boolean
+});
+var User = mongoose.model('users', usersSchema);
+var neil = new User ({email: 'nagar001@ucr.edu', password: 'neil', isOwner: false});
+neil.save(function(error) {
+  if(error) {
+    console.log(error);
+  } else {
+    console.log('successful');
+  }
+});
 
 
-app.post('/', function(req, res){
-
+app.get('/users', function(req, res){
+  User.find(function(err, users){
+    res.send(users);
+  })
 });
 
 app.listen(1738, function(){
