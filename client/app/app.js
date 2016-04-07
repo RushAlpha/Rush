6d8e542e6861844c871ac188c5d63905e4249e67
Brandon Willis <brandonlynnwillis@gmail.com>
2016-04-07 16:24:30 -0700

    [Feature] Finished sign/up client functionality

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,4 @@
-angular.module('Rush', ['ui.router'])
+angular.module('Rush', ['ui.router','Rush.services'])
 .config(function($stateProvider, $httpProvider, $urlRouterProvider){
 	console.log("config is running");
 	$urlRouterProvider.otherwise('signin')
@@ -16,9 +16,29 @@ angular.module('Rush', ['ui.router'])
 	})
 })
 
-.controller('authController', function($scope){
+.controller('authController', function($scope, AuthFactory){
 	$scope.logIn = function() {
+		AuthFactory.postSignIn($scope.username, $scope.password).then(function(data) {
+			if(err) {
+				console.log("LogIn post unsucessful!")
+				return;
+			}
+			console.log("LogIn post successful!")
+		});
 		console.log($scope.username, $scope.password, "USERNAMES AND PASSWORDS");
+	}
+	$scope.logUp = function() {
+		console.log("my owner box: ", $scope.isOwnerBox);
+		AuthFactory.postSignUp($scope.username, $scope.password, $scope.isOwnerBox).then(function(err,data) {
+			if(err) {
+				console.log("SignUp post unsucessful!")
+				return;
+			}
+			console.log("SignUp post successful!")
+		})
+	};
+	$scope.isOwnerBox = {
+		value: true
 	}
 })
 .run(function(){
--- client/app/services.js
+++ client/app/services.js
@@ -1,5 +1,5 @@
 angular.module('Rush.services', [])
-.factory('AuthFactory', function(){
+.factory('AuthFactory', function($http){
 
   var postSignIn = function(username, password) {
     return $http.post('/signin', {username:username, password: password})
@@ -7,5 +7,9 @@ angular.module('Rush.services', [])
 
   var postSignUp = function(username, password, isOwner) {
     return $http.post('/signup', {username: username, password: password, isOwner: isOwner})
-    }
+  }
+  return {
+    postSignIn: postSignIn,
+    postSignUp: postSignUp
+  }
 })
