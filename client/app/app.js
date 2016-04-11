44c743777f9f676eb5919190ac647a47945d49cb
Neil Agarwal <nagar001@ucr.edu>
2016-04-11 13:33:51 -0700

    [BugFix] codeCheck 4 missing ;'s & fixed console.logs

--- client/app/app.js
+++ client/app/app.js
@@ -24,13 +24,10 @@ angular.module('Rush', ['ui.router','rush-Services', 'owner-Module', 'consumer-M
 		controller: 'consumerController'
 	})
 });
-
 .controller('authController', function($geolocation, $scope, authFactory, $state){
-
 	$scope.location;
 	$scope.geoAddress = {};
 	var geocoder = new google.maps.Geocoder();
-
 
 	$scope.logIn = function() {
 		authFactory.postSignIn(
@@ -49,32 +46,26 @@ angular.module('Rush', ['ui.router','rush-Services', 'owner-Module', 'consumer-M
 	$scope.logUp = function() {
 		console.log('address: ', $scope.address)
 		geocoder.geocode( { "address": $scope.address }, function(results, status) {
-       if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-       	 $scope.location = results[0].geometry.location;
-       	 $scope.geoAddress.latitude = results[0].geometry.location.lat();
-       	 $scope.geoAddress.longitude = results[0].geometry.location.lng();
-       	 console.log("geoAddress#1: ", $scope.geoAddress);
-
-       	 		authFactory.postSignUp($scope.username, $scope.password, $scope.isOwnerBox.value, $scope.geoAddress)
-		.then(function(data) {
-				if (data.data === false){
-					$state.go('signin');
-				} else {
-					console.log("Successful logUp: ", $scope.username);
-					$state.go('owner');
-				}
-		})
-
-       }
+      if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+       	$scope.location = results[0].geometry.location;
+       	$scope.geoAddress.latitude = results[0].geometry.location.lat();
+       	$scope.geoAddress.longitude = results[0].geometry.location.lng();
+       	console.log("geoAddress#1: ", $scope.geoAddress);
+       	authFactory.postSignUp($scope.username, $scope.password, $scope.isOwnerBox.value, $scope.geoAddress)
+				.then(function(data) {
+					if (data.data === false){
+						$state.go('signin');
+					} else {
+						console.log("Successful logUp: ", $scope.username);
+						$state.go('owner');
+					}
+				});
+      }
 		});
 		console.log("geoAddress#2: ", $scope.geoAddress);
-
-	};
+	}
 	console.log($scope.address);
 	$scope.isOwnerBox = {
 		value: false
-	}
-
-
-
-})
+	};
+});
